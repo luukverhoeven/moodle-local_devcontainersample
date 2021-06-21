b52bd12b5992d5607e150cc488da6ba09f484d87
aoisupersix <aoisupersix@gmail.com>
2021-06-21 14:18:26 +0900

    [fix] plugin name in packaging script

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,11 +4,11 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 version=$($SCRIPT_DIR/get-version.sh)
 
 if [ -z "${1}" ]; then
-    outputFile="badge-${version}"
+    outputFile="devcontainersample-${version}"
 else
     outputFile=$1
 fi
 
 pushd $SCRIPT_DIR/../../
-zip -r $SCRIPT_DIR/../$outputFile badge -x "*.git/*" "*.devcontainer/*" "*.vscode/*" "*scripts/*" "*vendor/*" "*.DS_Store" "*__MACOSX*"
+zip -r $SCRIPT_DIR/../$outputFile devcontainersample -x "*.git/*" "*.devcontainer/*" "*.vscode/*" "*scripts/*" "*vendor/*" "*.DS_Store" "*__MACOSX*"
 popd
